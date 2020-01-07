ce8907deac3602f653a58b71b084f241a658eaf1
Alexander Tsidaev <a.tsidaev@gmail.com>
2020-01-08 03:51:12 +0500

    Replace resources

---
+++ src/ZXMAK2.Resources/ResourceImages.cs
@@ -0,0 +1,35 @@
+using System.Drawing;
+
+namespace ZXMAK2.Resources
+{
+    public class ResourceImages
+    {
+        public static Image DebuggerClose => Image.FromStream(typeof(ResourceImages).Assembly.GetManifestResourceStream("ZXMAK2.Resources.Icons.DebuggerClose.png"));
+        public static Image HardwareTapeAutoplay => Image.FromStream(typeof(ResourceImages).Assembly.GetManifestResourceStream("ZXMAK2.Resources.Icons.HardwareTapeAutoplay.png"));
+        public static Image DebuggerBreak => Image.FromStream(typeof(ResourceImages).Assembly.GetManifestResourceStream("ZXMAK2.Resources.Icons.DebuggerBreak.png"));
+        public static Image DebuggerContinue => Image.FromStream(typeof(ResourceImages).Assembly.GetManifestResourceStream("ZXMAK2.Resources.Icons.DebuggerContinue.png"));
+        public static Image DebuggerShowBreakpoints => Image.FromStream(typeof(ResourceImages).Assembly.GetManifestResourceStream("ZXMAK2.Resources.Icons.DebuggerShowBreakpoints.png"));
+        public static Image DebuggerShowNext => Image.FromStream(typeof(ResourceImages).Assembly.GetManifestResourceStream("ZXMAK2.Resources.Icons.DebuggerShowNext.png"));
+        public static Image DebuggerStepInto => Image.FromStream(typeof(ResourceImages).Assembly.GetManifestResourceStream("ZXMAK2.Resources.Icons.DebuggerStepInto.png"));
+        public static Image DebuggerStepOut => Image.FromStream(typeof(ResourceImages).Assembly.GetManifestResourceStream("ZXMAK2.Resources.Icons.DebuggerStepOut.png"));
+        public static Image DebuggerStepOver => Image.FromStream(typeof(ResourceImages).Assembly.GetManifestResourceStream("ZXMAK2.Resources.Icons.DebuggerStepOver.png"));
+        public static Image OsdFddRd => Image.FromStream(typeof(ResourceImages).Assembly.GetManifestResourceStream("ZXMAK2.Resources.Icons.OsdFddRd.png"));
+        public static Image OsdFddWr => Image.FromStream(typeof(ResourceImages).Assembly.GetManifestResourceStream("ZXMAK2.Resources.Icons.OsdFddWr.png"));
+        public static Image OsdHddRd => Image.FromStream(typeof(ResourceImages).Assembly.GetManifestResourceStream("ZXMAK2.Resources.Icons.OsdHddRd.png"));
+        public static Image HardwareTapeNext => Image.FromStream(typeof(ResourceImages).Assembly.GetManifestResourceStream("ZXMAK2.Resources.Icons.HardwareTapeNext.png"));
+        public static Image OsdPause => Image.FromStream(typeof(ResourceImages).Assembly.GetManifestResourceStream("ZXMAK2.Resources.Icons.OsdPause.png"));
+        public static Image HardwareTapePlay => Image.FromStream(typeof(ResourceImages).Assembly.GetManifestResourceStream("ZXMAK2.Resources.Icons.HardwareTapePlay.png"));
+        public static Image HardwareTapePrev => Image.FromStream(typeof(ResourceImages).Assembly.GetManifestResourceStream("ZXMAK2.Resources.Icons.HardwareTapePrev.png"));
+        public static Image HardwareTapeRecord => Image.FromStream(typeof(ResourceImages).Assembly.GetManifestResourceStream("ZXMAK2.Resources.Icons.HardwareTapeRecord.png"));
+        public static Image HardwareTapeRewind => Image.FromStream(typeof(ResourceImages).Assembly.GetManifestResourceStream("ZXMAK2.Resources.Icons.HardwareTapeRewind.png"));
+        public static Image HardwareTapeTraps => Image.FromStream(typeof(ResourceImages).Assembly.GetManifestResourceStream("ZXMAK2.Resources.Icons.HardwareTapeTraps.png"));
+        public static Image HardwareTapePause => Image.FromStream(typeof(ResourceImages).Assembly.GetManifestResourceStream("ZXMAK2.Resources.Icons.HardwareTapePause.png"));
+        public static Image Stop_real_16x16 => Image.FromStream(typeof(ResourceImages).Assembly.GetManifestResourceStream("ZXMAK2.Resources.Icons.Stop_real_16x16.png"));
+        public static Image OsdTapeRd => Image.FromStream(typeof(ResourceImages).Assembly.GetManifestResourceStream("ZXMAK2.Resources.Icons.OsdTapeRd.png"));
+        public static Image KeyboardHelp => Image.FromStream(typeof(ResourceImages).Assembly.GetManifestResourceStream("ZXMAK2.Resources.Pictures.KeyboardHelp.png"));
+        public static Image IconDebugger => Image.FromStream(typeof(ResourceImages).Assembly.GetManifestResourceStream("ZXMAK2.Resources.Icons.IconDebugger.ico"));
+        public static Image IconApp => Image.FromStream(typeof(ResourceImages).Assembly.GetManifestResourceStream("ZXMAK2.Resources.Icons.IconApp.ico"));
+        public static Image ImageZxLogo => Image.FromStream(typeof(ResourceImages).Assembly.GetManifestResourceStream("ZXMAK2.Resources.Pictures.ZXLogo.gif"));
+
+    }
+}
