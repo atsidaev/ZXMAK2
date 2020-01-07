7bdd873c7d03137595819a24fe104ceec460d28f
Alexander Tsidaev <a.tsidaev@gmail.com>
2020-01-08 04:30:23 +0500

    Continue craziness

--- src/ZXMAK2.Host/Entities/IconDescriptor.cs
+++ src/ZXMAK2.Host/Entities/IconDescriptor.cs
@@ -8,8 +8,6 @@ namespace ZXMAK2.Host.Entities
 {
     public class IconDescriptor : IIconDescriptor
 	{
-		private readonly byte[] m_iconData;
-
         public string Name { get; private set; }
         public Size Size { get; private set; }
 		public bool Visible { get; set; }
@@ -17,36 +15,12 @@ namespace ZXMAK2.Host.Entities
         public IconDescriptor(string iconName, Image iconImage)
         {
             Name = iconName;
-            Size = iconImage.Size;
-            using (var stream = new MemoryStream())
-            {
-                iconImage.Save(stream, ImageFormat.Png);
-                m_iconData = stream.ToArray();
-            }
+            Size = new Size(0, 0);
         }
         
-        public IconDescriptor(string iconName, Stream iconStream)
-		{
-            if (iconStream == null)
-            {
-                throw new FileNotFoundException(
-                    string.Format(
-                        "Icon stream '{0}' not found",
-                        iconName));
-            }
-            Name = iconName;
-			m_iconData = new byte[iconStream.Length];
-			iconStream.Read(m_iconData, 0, m_iconData.Length);
-			using (var stream = GetImageStream())
-            using (var bitmap = new Bitmap(stream))
-            {
-                Size = bitmap.Size;
-            }
-		}
-
 		public Stream GetImageStream()
 		{
-			return new MemoryStream(m_iconData);
+			return new MemoryStream(new byte[0]);
 		}
 	}
 }
